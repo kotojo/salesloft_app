187b27280a303054880594d9f9d2e16032531231
Bruce Kellerman <brucek17@hotmail.com>
2018-06-11 21:24:48 -0400

    setup create react app with proxy calls to server

--- server/app.js
+++ server/app.js
@@ -54,5 +54,5 @@ app.use(function(err, req, res, next) {
   });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`listening on port ${port}`));
